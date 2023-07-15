4147c2d5d843bfb19a04ab043d415b751c2458d9
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-07-15 22:41:41 +0200

    stor-e_BackEnd: setting up project done

--- src/main/java/com/ascendio/store_backend/util/Converter.java
+++ src/main/java/com/ascendio/store_backend/util/Converter.java
@@ -6,37 +6,38 @@ import com.ascendio.store_backend.dto.StoryRequestDto;
 import com.ascendio.store_backend.dto.StoryResponseDto;
 import com.ascendio.store_backend.model.Story;
 import com.ascendio.store_backend.model.StoryBook;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
 public class Converter {
 
-    public Story fromStoryRequestDto(StoryRequestDto dto) {
+    private Converter() {
+    }
+
+    public static Story fromStoryRequestDto(StoryRequestDto dto) {
         return new Story();
     }
 
-    public List<StoryResponseDto> toStoryResponseDtoList(List<Story> stories) {
+    public static List<StoryResponseDto> toStoryResponseDtoList(List<Story> stories) {
         List<StoryResponseDto> storyResponses = null;
         return storyResponses;
     }
 
-    public StoryResponseDto toStoryResponseDto(Story story) {
+    public static StoryResponseDto toStoryResponseDto(Story story) {
         StoryResponseDto storyResponse = null;
         return storyResponse;
     }
 
-    public StoryBook fromStoryBookRequestDto(StoryBookRequestDto dto) {
+    public static StoryBook fromStoryBookRequestDto(StoryBookRequestDto dto) {
         return new StoryBook();
     }
 
-    public List<StoryBookResponseDto> toStoryBookResponseDtoList(List<StoryBook> storyBooks) {
+    public static List<StoryBookResponseDto> toStoryBookResponseDtoList(List<StoryBook> storyBooks) {
         List<StoryBookResponseDto> storyBookResponses = null;
         return storyBookResponses;
     }
 
-    public StoryBookResponseDto toStoryBookResponseDto(StoryBook storyBook) {
+    public static StoryBookResponseDto toStoryBookResponseDto(StoryBook storyBook) {
         StoryBookResponseDto storyBookResponse = null;
         return storyBookResponse;
     }
