e00f2c513c3c126a6420716b0e16c01a477f31f3
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-07-18 00:47:53 +0200

    stor-e_BackEnd: Implemented Dall-E API GET request, tested, working properly.

--- src/main/java/com/ascendio/store_backend/StorEApplication.java
+++ src/main/java/com/ascendio/store_backend/StorEApplication.java
@@ -2,7 +2,6 @@ package com.ascendio.store_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class StorEApplication {
--- src/main/java/com/ascendio/store_backend/dto/DalleImageResponse.java
+++ src/main/java/com/ascendio/store_backend/dto/DalleImageResponse.java
@@ -1,8 +1,4 @@
 package com.ascendio.store_backend.dto;
 
 public record DalleImageResponse(String storyLine, String url) {
-//    @Override
-//    public String url() {
-//        return url;
-//    }
 }
