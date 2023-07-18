7140df0446a04b766c29c4b4831d18a51f976df9
Robbie-Corcoran <codewithrobbie@gmail.com>
2023-07-18 21:52:01 +0200

    Controller implimented. App running. Not tested yet.

--- src/main/java/com/ascendio/store_backend/controller/FavouritesController.java
+++ src/main/java/com/ascendio/store_backend/controller/FavouritesController.java
@@ -1,9 +1,29 @@
 package com.ascendio.store_backend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ascendio.store_backend.model.Favourite;
+import com.ascendio.store_backend.model.StoryBook;
+import com.ascendio.store_backend.service.FavouritesService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/favourites")
 public class FavouritesController {
+
+    private final FavouritesService favouritesService;
+
+    public FavouritesController(FavouritesService favouritesService) {
+        this.favouritesService = favouritesService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Favourite> saveFavourite(@RequestBody StoryBook storyBook) {
+        if (storyBook == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return new ResponseEntity<>(favouritesService.saveFavourite(storyBook), HttpStatus.CREATED);
+    }
 }
